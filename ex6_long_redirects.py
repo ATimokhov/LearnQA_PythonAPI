6e5ac94175989166eb19b5b229815174448eeee4
Andrey Timokhov <96545293+ATimokhov@users.noreply.github.com>
2022-01-05 16:09:32 +0300

    ex6

--- ex6_long_redirects.py
+++ ex6_long_redirects.py
@@ -1,5 +1,5 @@
 import requests
-response1 = requests.get(" https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+response1 = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
 redirects = response1.history
 number_of_elements = len(redirects)
 print(f'Кол-во редиректов: {number_of_elements}')
--- ex7_method.py
+++ ex7_method.py
@@ -1,1 +1,8 @@
 import requests
+# https://playground.learnqa.ru/ajax/api/compare_query_type
+
+response_post = requests.delete("https://playground.learnqa.ru/api/compare_query_type")
+
+
+print(response_post.text)
+
